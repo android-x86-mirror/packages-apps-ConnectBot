546d9cde405df3bf701cff7c8a2dfc469d9f6277
Kenny Root <kenny@the-b.org>
2010-03-05 20:05:42 +0800

    Update to r482
    Make sure EastAsianWidth JNI is working before using it.

--- src/org/connectbot/ColorsActivity.java
+++ src/org/connectbot/ColorsActivity.java
@@ -1,20 +1,19 @@
 /*
-	ConnectBot: simple, powerful, open-source SSH client for Android
-	Copyright (C) 2007-2008 Kenny Root, Jeffrey Sharkey
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * ConnectBot: simple, powerful, open-source SSH client for Android
+ * Copyright 2007 Kenny Root, Jeffrey Sharkey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.connectbot;
 
@@ -308,6 +307,7 @@ public class ColorsActivity extends Activity implements OnItemClickListener, OnC
 		reset.setIcon(android.R.drawable.ic_menu_revert);
 		reset.setOnMenuItemClickListener(new OnMenuItemClickListener() {
 			public boolean onMenuItemClick(MenuItem arg0) {
+				// Reset each individual color to defaults.
 				for (int i = 0; i < Colors.defaults.length; i++) {
 					if (mColorList.get(i) != Colors.defaults[i]) {
 						hostdb.setGlobalColor(i, Colors.defaults[i]);
@@ -315,6 +315,13 @@ public class ColorsActivity extends Activity implements OnItemClickListener, OnC
 					}
 				}
 				mColorGrid.invalidateViews();
+
+				// Reset the default FG/BG colors as well.
+				mFgSpinner.setSelection(HostDatabase.DEFAULT_FG_COLOR);
+				mBgSpinner.setSelection(HostDatabase.DEFAULT_BG_COLOR);
+				hostdb.setDefaultColorsForScheme(HostDatabase.DEFAULT_COLOR_SCHEME,
+						HostDatabase.DEFAULT_FG_COLOR, HostDatabase.DEFAULT_BG_COLOR);
+
 				return true;
 			}
 		});
